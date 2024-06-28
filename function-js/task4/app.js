8e3cfad0279f14d9f52e0b03442e253806aac333
ArtsemiKot <kot_artemiy@mail.ru>
2024-06-28 23:37:01 +0300

    homework

---
+++ function-js/task4/app.js
@@ -0,0 +1,7 @@
+// 4. Напишите функцию, которая принимает строку и возвращает ее, но все буквы переведены
+// в нижний регистр.
+
+
+const stringCamelCase = (str) => str.toLowerCase()
+
+console.log(stringCamelCase('I aM deVeLoPer'))
